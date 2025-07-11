5f5556feebe9d33d7c372715914bbdb608297e74
Hasan-Arda-Gunes <hardagunes@gmail.com>
2025-07-11 15:43:19 +0300

    Register and login with accessTokens

--- BackEnd/schemas/user_schema.py
+++ BackEnd/schemas/user_schema.py
@@ -8,6 +8,14 @@ class UserCreate(BaseModel):
 class UserRead(BaseModel):
     id: int
     username: str
-    
+
     class Config:
         orm_mode = True  # Enables compatibility with SQLAlchemy models
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
--- BackEnd/service/user_service.py
+++ BackEnd/service/user_service.py
@@ -3,19 +3,20 @@ from sqlalchemy.orm import Session
 from repository import user_repository
 from schemas.user_schema import UserCreate
 from models import User
+from auth import hash_password
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def create_user_service(db: Session, user_data: UserCreate) -> User:
+def create_user(db: Session, user_data: UserCreate) -> User:
     existing_user = user_repository.get_user_by_username(db, user_data.username)
     if existing_user:
         raise ValueError("Username already taken")
     
-    hashed_password = pwd_context.hash(user_data.password)
+    hashed_pwd = hash_password(user_data.password)
 
     new_user = User(
         username=user_data.username,
-        password=hashed_password
+        password=hashed_pwd
     )
 
     created_user = user_repository.create_user(db, new_user)
